1c20162de9fee65e34f8fc79e0981304499e5714
Rita Huang <1992ritahuang@gmail.com>
2025-01-19 13:53:51 +0800

    init

---
+++ TodoApp/ToDoItem.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoItem.swift
+//  TodoApp
+//
+//  Created by Rita Huang on 2025/1/19.
+//
+
+import Foundation
+
+struct ToDoItem: Identifiable, Codable {
+    let id: UUID
+    var title: String
+    var isFinish: Bool
+    
+    init(title: String, isFinish: Bool) {
+        self.id = UUID()
+        self.title = title
+        self.isFinish = isFinish
+    }
+}
---
+++ TodoApp/ToDoViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoViewModel.swift
+//  TodoApp
+//
+//  Created by Rita Huang on 2025/1/19.
+//
+
+import Foundation
+import Combine
+
+class ToDoViewModel: ObservableObject {
+
+}
